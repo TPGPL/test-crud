da973904a4423ce9de900cfdf08a66b3b11b17df
TPGPL <TPGPL@users.noreply.github.com>
2023-10-14 01:16:54 +0200

    Initial commit

---
+++ src/test/java/pl/edu/pw/mwotest/MwoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.mwotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwoTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
