6b5d1034e78df3d8de1760f75d30a517de53537a
Damian Denc <01169551@pw.edu.pl>
2023-10-27 17:06:03 +0200

    ProductService Integration Tests

--- src/main/java/pl/edu/pw/mwotest/models/Product.java
+++ src/main/java/pl/edu/pw/mwotest/models/Product.java
@@ -9,24 +9,30 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.Builder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
+@Builder
 @Table(name = "Products")
 @NotNull(message = "The product must not be null.")
 public class Product {
+    public static final int MAX_NAME_LENGTH = 100;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "product_id")
     private int id;
     @NotNull(message = "The product name must not be null.")
-    @Size(min = 2, max = 100, message = "The product name must be between 2 and 100 characters.")
+    @Size(min = 2, max = MAX_NAME_LENGTH, message = "The product name must be between 2 and 100 characters.")
+    @Column(unique = true, nullable = false)
     private String name;
     @Positive(message = "The product price must be higher than 0.")
     private double price;
@@ -34,4 +40,17 @@ public class Product {
     private int stockQuantity;
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderLine> orderLines = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
