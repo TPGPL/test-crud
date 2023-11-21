6e1c422ce6323e4ded71991e8b69458cd58ec70c
TPGPL <TPGPL@users.noreply.github.com>
2023-11-21 18:36:05 +0100

    Add cross origin

--- src/main/java/pl/edu/pw/mwotest/controllers/ClientController.java
+++ src/main/java/pl/edu/pw/mwotest/controllers/ClientController.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class ClientController {
     private final ClientService service;
 
--- src/main/java/pl/edu/pw/mwotest/controllers/OrderController.java
+++ src/main/java/pl/edu/pw/mwotest/controllers/OrderController.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class OrderController {
     private final OrderService service;
 
