28b095ece1e3f922c8c117f267d41ecfe7568880
Szymon Ochnio <szymonochnio15@gmail.com>
2023-11-24 22:22:24 +0100

    Add more scenarios for product service.

--- src/test/java/pl/edu/pw/mwotest/cucumber_it/ProductServiceIntegrationTestsSteps.java
+++ src/test/java/pl/edu/pw/mwotest/cucumber_it/ProductServiceIntegrationTestsSteps.java
@@ -1,20 +1,28 @@
 package pl.edu.pw.mwotest.cucumber_it;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import jakarta.validation.ConstraintViolationException;
 import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.ThrowableAssert;
+import org.springframework.dao.DataIntegrityViolationException;
 import pl.edu.pw.mwotest.models.Product;
 import pl.edu.pw.mwotest.services.ProductService;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @RequiredArgsConstructor
 public class ProductServiceIntegrationTestsSteps {
     private static Product product;
+    private static ThrowableAssert.ThrowingCallable actionToCall;
+    private static int requestedId;
     private final ProductService productService;
 
     private static String proxyBlank(Object obj) {
@@ -43,6 +51,7 @@ public class ProductServiceIntegrationTestsSteps {
                 .name(proxyBlank(data.get(0).get("name")))
                 .price(proxyZero(data.get(0).get("price"), Double.class).doubleValue())
                 .stockQuantity(proxyZero(data.get(0).get("stockQuantity"), Integer.class).intValue())
+                .orderLines(new ArrayList<>())
                 .build();
     }
 
@@ -58,5 +67,68 @@ public class ProductServiceIntegrationTestsSteps {
         assertThat(product.getName()).isEqualTo(retrievedProduct.getName());
         assertThat(product.getPrice()).isEqualTo(retrievedProduct.getPrice());
         assertThat(product.getStockQuantity()).isEqualTo(retrievedProduct.getStockQuantity());
+    }
+
+    @When("I try to create a product")
+    public void iTryToCreateAProduct() {
+        actionToCall = () -> productService.createProduct(product);
+    }
+
+    @Then("product creation throws Validation Exception")
+    public void productCreationThrowsValidationException() {
+        assertThatThrownBy(actionToCall).isInstanceOf(ConstraintViolationException.class);
+    }
+
+
+    @And("product is created if not exists")
+    public void productIsCreatedIfNotExists() {
+        try {
+            requestedId = productService.createProduct(product).getId();
+        } catch (DataIntegrityViolationException ignored) {
+            Optional<Product> existingProduct = StreamSupport.stream(productService.getAllProducts().spliterator(), false)
+                    .filter(existing -> existing.getName().equals(product.getName()))
+                    .findFirst();
+
+            existingProduct.ifPresentOrElse(
+                    existing -> requestedId = existing.getId(),
+                    () -> {
+                        throw new RuntimeException("Product not found");
+                    }
+            );
+        }
+    }
+
+    @When("I create product with duplicated name")
+    public void iCreateProductWithDuplicatedName() {
+        var product2 = Product.builder()
+                .name(product.getName())
+                .price(product.getPrice())
+                .stockQuantity(product.getStockQuantity())
+                .orderLines(new ArrayList<>())
+                .build();
+        actionToCall = () -> productService.createProduct(product2);
+    }
+
+    @Then("product creation throws Unique Constraint Exception")
+    public void productCreationThrowsUniqueConstraintException() {
+        assertThatThrownBy(actionToCall).isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @Given("product id is max+{int}")
+    public void productIdIsMax(int arg0) {
+        Stream<Integer> productIDs = StreamSupport.stream(productService.getAllProducts().spliterator(), false)
+                .map(Product::getId);
+        int maxProductId = productIDs.max(Comparator.naturalOrder()).orElse(0);
+        requestedId = maxProductId + arg0;
+    }
+
+    @When("I read product by id")
+    public void iReadProductById() {
+        product = productService.getProduct(requestedId);
+    }
+
+    @Then("product is null")
+    public void productIsNull() {
+        assertThat(product).isNull();
     }
 }
--- src/test/resources/features_it/ProductServiceIntegrationTests.feature
+++ src/test/resources/features_it/ProductServiceIntegrationTests.feature
@@ -11,3 +11,32 @@ Feature: Product service test
       | Piwo  | 2.99  | 2136          |
       | Fajki | 15.99 | 420           |
       | Mleko | 4.49  | 16            |
+
+  Scenario Template: Create product with invalid data
+    Given product data as below
+      | name   | price   | stockQuantity   |
+      | <name> | <price> | <stockQuantity> |
+    When I try to create a product
+    Then product creation throws Validation Exception
+    Examples:
+      | name | price | stockQuantity |
+      |      | 2.99  | 420           |
+      | P    | 2.99  | 420           |
+      | Piwo | -2.99 | 420           |
+      | Piwo |       | 420           |
+      | Piwo | 0     | 420           |
+      | Piwo | 2.99  | -1            |
+
+  Scenario: Create product with duplicated name
+    Given product data as below
+      | name | price | stockQuantity |
+      | Piwo | 2.99  | 420           |
+    And product is created if not exists
+    When I create product with duplicated name
+    Then product creation throws Unique Constraint Exception
+
+  Scenario: Read non existing product
+    Given product id is max+1
+    When I read product by id
+    Then product is null
+
